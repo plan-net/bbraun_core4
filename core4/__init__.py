465995e7c40a5a6f890cc5e982257e810af3d13d
Bastian Zimmermann <bastizauss@gmx.de>
2025-08-01 14:53:13 +0200

    Update __init__.py

--- core4/__init__.py
+++ core4/__init__.py
@@ -5,7 +5,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-__project__ = 'bbraun'
+__project__ = 'core4'
 __version__ = "0.7.4"
 __built__ = "2023-05-18 20:49:46.056000"
 name = 'bbraun_linkbuilder'
