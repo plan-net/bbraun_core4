d8aa233ceb5cf0d5905b79c62e18cfb69117a709
Bastian Zimmermann <bastizauss@gmx.de>
2025-08-01 14:57:58 +0200

    Update __init__.py

--- core4/__init__.py
+++ core4/__init__.py
@@ -6,7 +6,7 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 __project__ = 'core4'
-__version__ = "0.7.4"
+__version__ = "linkbuilder_0.7.4"
 __built__ = "2023-05-18 20:49:46.056000"
 name = 'bbraun_linkbuilder'
 title = 'B.Braun Linkbuilder'
